4449d7f709d872227977d0f802453e3c77848b6d
yopparay <zahry.prz@gmail.com>
2025-01-28 15:27:01 +0100

    add 404 page

--- auth/settings.py
+++ auth/settings.py
@@ -26,7 +26,7 @@ from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ath-service-64vx.onrender.com', '127.0.0.1', 'localhost']
 
